87a26535674f1bf8aa2c5f730256f0adef670c75
FDMJ <martins_julian@gmx.de>
2024-05-12 13:34:24 +0200

    added db.json

---
+++ src/views/EventListView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  {
+    id: 512312,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find your new feline friend at this event.',
+    location: 'Meow Town',
+    date: 'January 28, 2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee'
+  },
+  {
+    id: 894654,
+    category: 'food',
+    title: 'Community Gardening',
+    description: 'Join us as we tend to the community edible plants.',
+    location: 'Flora City',
+    date: 'March 12, 2022',
+    time: '10:00',
+    petsAllowed: true,
+    organizer: 'Fern Pollin'
+  },
+  {
+    id: 791232,
+    category: 'sustainability',
+    title: 'Beach Cleanup',
+    description: 'Help pick up trash along the shore.',
+    location: 'Playa del Carmen',
+    date: 'July 22, 2022',
+    time: '11:00',
+    petsAllowed: true,
+    organizer: 'Carey Wales'
+  }
+])
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
